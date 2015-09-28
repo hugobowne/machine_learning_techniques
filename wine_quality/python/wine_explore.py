e49da2fbbca41e790a0950ca799f47d54dfbb4e9
Fernando Carrillo <fernando@carrillo.at>
2015-09-27 20:55:43 -0400

    Added some base estimators.

--- wine_quality/python/wine_explore.py
+++ wine_quality/python/wine_explore.py
@@ -2,13 +2,13 @@ __author__ = "Fernando Carrillo"
 __email__ = "fernando at carrillo.at"
 
 from matplotlib import pyplot as plt
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, Normalizer, RobustScaler
 from sklearn.decomposition import PCA
 from sklearn.manifold import Isomap, TSNE, LocallyLinearEmbedding, SpectralEmbedding, MDS
 
 import pandas as pd
 
-def plot2d(X, y, scale=True, embedding='pca', title=''): 
+def plot2d(X, y, scale=True, normalize=False, embedding='pca', title=''): 
 	"""
 	Plot data transformed into two dimensions by PCA. 
 	PCA transforms into a new embedding dimension such that 
@@ -21,32 +21,80 @@ def plot2d(X, y, scale=True, embedding='pca', title=''):
 		scaler = StandardScaler()
 		X = scaler.fit_transform(X)
 
+	if (normalize): 
+		normalizer = Normalizer(norm='l2')
+		X = normalizer.fit_transform(X)
+		
 	if (embedding is 'pca'): 
 		pca = PCA(n_components=2)
-		pca.fit(X)
-		X_transformed = pca.transform(X)
+		X_transformed = pca.fit_transform(X)
 	elif (embedding is 'isomap'):
 		isomap = Isomap(n_components=2, n_neighbors=20)
 		X_transformed = isomap.fit_transform(X)
-	elif (embedding is 'tsne'): 
-		t_sne = TSNE(n_components=2)
-		X_transformed = t_sne.fit_transform(X)
 	elif (embedding is 'lle' ): 
 		lle = LocallyLinearEmbedding(n_components=2, n_neighbors=5)
 		X_transformed = lle.fit_transform(X)
+	elif (embedding is 'tsne'): 
+		t_sne = TSNE(n_components=2)
+		X_transformed = t_sne.fit_transform(X)
 	elif (embedding is 'spectral'): 
 		se = SpectralEmbedding(n_components=2)
 		X_transformed = se.fit_transform(X)
 	elif (embedding is 'mds'):
 		mds = MDS(n_components=2)
 		X_transformed = mds.fit_transform(X)
+	elif (embedding is 'gallery'): 
+		plt.figure(1)
+		
+		plt.subplot(231)
+		plt.title('pca')
+		X_t = PCA(n_components=2).fit_transform(X)
+		plt.scatter(X_t[:,0 ], X_t[:, 1], c=y)
+
+		plt.subplot(232)
+		plt.title('isomap')
+		X_t = Isomap(n_neighbors=20).fit_transform(X)
+		plt.scatter(X_t[:,0 ], X_t[:, 1], c=y)
+
+		plt.subplot(233)
+		plt.title('lle')
+		X_t = LocallyLinearEmbedding(n_neighbors=20).fit_transform(X)
+		plt.scatter(X_t[:,0 ], X_t[:, 1], c=y)
+
+		plt.subplot(234)
+		plt.title('tsne')
+		X_t = TSNE().fit_transform(X)
+		plt.scatter(X_t[:,0 ], X_t[:, 1], c=y)
+
+		plt.subplot(235)
+		plt.title('spectral')
+		X_t = SpectralEmbedding().fit_transform(X)
+		plt.scatter(X_t[:,0 ], X_t[:, 1], c=y)
+
+		plt.subplot(236)
+		plt.title('mds')
+		X_t = MDS().fit_transform(X)
+		plt.scatter(X_t[:,0 ], X_t[:, 1], c=y)
+
+		plt.suptitle('Gallery transforms ' + title)
+
+		return plt
 	else:
 		raise ValueError("Choose between pca, isomap and tsne")
 
 	plt.title(title + ' ' + embedding + ' plot')
 	sc = plt.scatter(X_transformed[:, 0], X_transformed[:, 1], c=y)
 	plt.colorbar(sc)
-	plt.show()
+	return plt
+
+def plot2d_gallery(self, X, y, scale=True): 
+	if (scale): 
+		scaler = StandardScaler()
+		X = scaler.fit_transform(X)
+
+	plt.figure(1)
+	plt.subplot(231)
+	plt.axes.append()
 
 def pairs(X, y, title): 
 	"""
--- wine_quality/python/wine_main.py
+++ wine_quality/python/wine_main.py
@@ -8,6 +8,17 @@ from matplotlib import pyplot as plt
 import numpy as np 
 import pandas as pd
 
+from sklearn.pipeline import Pipeline
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.grid_search import GridSearchCV
+from sklearn import cross_validation
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+
 data = WineData('../winequality-red.csv', '../winequality-white.csv')
 
 X_red, y_red = data.load_red()
@@ -15,21 +26,64 @@ X_white, y_white = data.load_white()
 
 ###############################
 # Explore data 
-# 1. Plot in 2d, color code classes 
-#	-> few very strong outliers: consider removing. 
+# 1. Plot in 2d, color code classes: 
+#	-> no simple low dimension linear separation
 # 2. Plot paris 
 #	-> correlation: transform data or use regularized methods
 #	-> non-normal distributed featues: Box-Cox transform
 ###############################
-#pairs(X_red, y_red, 'Red wine')
-#pairs(X_white, y_white, 'White wine')
+do_plot = False 
+if (do_plot): 
+	plot2d(X_red, y_red, embedding='gallery', title='Red wine').savefig('../data/red_whine_2d_gallery.png')
+	plot2d(X_white, y_white, embedding='gallery', title='White wine').savefig('../data/white_whine_2d_gallery.png')
+	pairs(X_red, y_red, 'Red wine')
+	pairs(X_white, y_white, 'White wine')
 
-# Plot in 2d. Both have strong outliers. Consider removing. 
-plot2d(X_red, y_red, scale=True, embedding='pca', title='Red wine')
-plot2d(X_red, y_red, scale=True, embedding='isomap', title='Red wine')
-plot2d(X_red, y_red, scale=True, embedding='lle', title='Red wine')
-plot2d(X_red, y_red, scale=True, embedding='spectral', title='Red wine')
-plot2d(X_red, y_red, scale=True, embedding='mds', title='Red wine')
-plot2d(X_red, y_red, scale=True, embedding='tsne', title='Red wine')
+###############################
+# Classification 
+# Prepare data 
+###############################
+X = X_red
+y = y_red
+X_train, X_holdout, y_train, y_holdout = train_test_split(X, y, random_state=23, test_size=0.1)
 
-#plot2d(X_white, y_white, 'White wine')
+###############################
+# What is a base level classification? 
+#
+# Would be cool, to implement a simple zero  
+# Not sure what to use, i) nearest neighbor classification, ii) logisitic regression, iii) linear SVM iv) Naive Bayes ? 
+# Best score: NN: 0.63 (n_neighbors=1), SVC: 0.57 (C=10), Logisitic regression 0.59 (C=1)
+###############################
+do_base_rate = True
+if (do_base_rate): 
+	def train_plot_single_param_grid(steps, param_grid, name): 
+		clf = Pipeline(steps)
+		grid_search = GridSearchCV(clf, param_grid=param_grid, cv=5, verbose=1).fit(X_train, y_train)
+		print( ('Best score %s of estimator %s') % (grid_search.best_score_, grid_search.best_estimator_))
+		plt.figure(1)
+		plt.title("Learning curve " + name)
+		plt.plot([c.mean_validation_score for c in grid_search.grid_scores_], label="validation error")
+		plt.show()
+
+	steps = [('scale', StandardScaler()), ('nb', GaussianNB())]
+	scores = cross_validation.cross_val_score(Pipeline(steps), X_train, y_train, cv=5)
+	print(('Best score %s') % (scores.mean()))
+
+	steps = [('scale', StandardScaler()), ('nn', KNeighborsClassifier())]
+	param_grid = {'nn__n_neighbors': [1, 2, 4, 8, 32, 64, 128, 256, 512]}
+	train_plot_single_param_grid(steps, param_grid, 'Nearest Neighbor')
+
+	steps = [('scale', StandardScaler()), ('svc', LinearSVC())]
+	param_grid = {'svc__C': 10. ** np.arange(-3, 4)}
+	train_plot_single_param_grid(steps, param_grid, 'Linear SVC')
+
+	steps = [('scale', StandardScaler()), ('logistic', LogisticRegression(multi_class='multinomial', solver='lbfgs'))]
+	param_grid = {'logistic__C': 10. ** np.arange(-3, 4)}
+	train_plot_single_param_grid(steps, param_grid, 'Logistic')
+
+###############################
+# What is a base level classification? 
+# Not sure what to use, i) nearest neighbor classification, ii) logisitic regression, iii) linear SVM? 
+# Best score: NN: 0.63 (n_neighbors=1), SVC: 0.57 (C=10), Logisitic regression 0.59 (C=1)
+###############################
+
